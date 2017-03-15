2ee3cdc7e484a001e8f4bac8aeff4c1c5539c05d
Joel Pereira <jap.joel@gmail.com>
2017-03-15 22:24:40 +0530

    completed shell script, yet to test

--- batterymod.sh
+++ batterymod.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
+#Copy this to a separate location for cron to use, so ongoing development won't affect it
 DFOLDER=~/Stuff/Myprojects/batterymod/test/  #for debug, change
-NUMBUH=0
-SESSION_NO=0
+
 mkdir -p $DFOLDER
 
 #Battery status
-BATTSTATUS="$(pmset -g ps | grep -o  '..%' | sed -e 's/%//g')"
+BATTSTATUS=$(pmset -g ps | grep -o  '..%' | sed -e 's/%//g') #quotes removed here, not tested
 LOWBATTERY=10
-if  [[ "$BATTSTATUS" -lt "$LOWBATTERY" &&  "$BATTSTATUS" -ne "00" ]]
+if [[ "$BATTSTATUS" -lt "$LOWBATTERY" &&  "$BATTSTATUS" -ne "00" ]]
 then
     osascript -e "display notification \"Battery power is lower than $LOWBATTERY percent.\""
 fi
@@ -24,3 +24,24 @@ else
 fi
 
 #Screenshot
+NUMBUH=0
+SESSION_NO=0
+if [[ "$FINAL_SCREENSTATE" -eq "on" ]]
+then 
+    if [[ "$NUMBUH" -eq "10" ]] #at smaller query-time values, there's an additional second to account for running this itself; 
+                                #so a 5 second query would required 100 not 120 for every ten minutes
+    then
+        SSDATE=$(date '+%Y-%m-%d_%H-%M')
+        /usr/sbin/screencapture -x "$DFOLDER""$SSDATE".png
+        echo $(date) screenshot "$DFOLDER""$SSDATE".png attempted >>"$DFOLDER"log.txt
+        osascript -e "display notification \"Logged.\""
+        NUMBUH=1
+        SESSION_NO=$(($SESSION_NO + 1))
+    else
+        NUMBUH=$(($NUMBUH + 1))
+    fi
+else
+    echo $(date) total sessions was $SESSION_NO  and lost so many units: $NUMBUH  >>"$DFOLDER"log.txt
+    NUMBUH=0
+    SESSION_NO=0
+fi

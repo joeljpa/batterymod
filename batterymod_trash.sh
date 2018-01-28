a86becf764b34d29c580f540e1a12c73bbb03048
Joel Pereira <jap.joel@gmail.com>
2018-01-28 14:53:57 +0530

    trash script: add rmtrash

--- batterymod_trash.sh
+++ batterymod_trash.sh
@@ -2,3 +2,4 @@
 IMG_TOTAL=$(ls -lt | grep .png | wc -l)
 IMG_READY=$((($IMG_TOTAL * 3) / 4))
 ls -t | grep .png | tail -n $IMG_READY | xargs du -ch
+#ls -t | grep .png | tail -n $IMG_READY | xargs rmtrash
